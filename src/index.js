97153bcc67ea88d79226895614b82fc9c970f766
Kevin Auchoybur <k.auchoybur@highco-data.fr>
2018-08-14 14:03:50 +0200

    Modification d'un state

--- src/index.js
+++ src/index.js
@@ -16,12 +16,28 @@ class App extends React.Component{
         text : sampleText
      };
 
+     
+     //Fonction fleche pour éditer le text
+     editText = (event) => {
+        const text = event.target.value;
+        console.log(text);
+        //Mettre à jour un state
+        this.setState({text});
+     };
+
     render(){
         return(
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6">
-                    <textarea value= {this.state.text} rows="35" className="form-control"></textarea>
+                    <textarea 
+                    value= {this.state.text} 
+                    rows="35" 
+                    className="form-control"
+                    //Chaque changement, utiliser l'attribut on change
+                    onChange= {(e) => this.editText(e)}
+                    >
+                    </textarea>
             </div>
                 <div className="col-sm-6">
                 <h1>Résultats</h1>
