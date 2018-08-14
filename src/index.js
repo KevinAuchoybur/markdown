334885464ae72ac0a423c874039ea0d7eea25392
Kevin Auchoybur <k.auchoybur@highco-data.fr>
2018-08-14 16:41:09 +0200

    Markdown sans localStorage

--- src/index.js
+++ src/index.js
@@ -6,24 +6,30 @@ import'./style/css/bootstrap.min.css';
 import'./index.css';
 //JS perso
 import {sampleText} from'./sampleText';
+//Import du Markdown
+import marked from 'marked';
 
 
 class App extends React.Component{
 
     //Creation d'un state
-
     state = {
         text : sampleText
      };
 
-     
-     //Fonction fleche pour éditer le text
+     //Fonction flechée pour éditer le text
      editText = (event) => {
         const text = event.target.value;
         console.log(text);
         //Mettre à jour un state
         this.setState({text});
      };
+
+     //Fonction qui va creer la traduction
+     renderText = (text) => {
+        const renderText = marked(text,{sanitize:true});
+        return {__html : renderText}
+     }
 
     render(){
         return(
@@ -40,8 +46,7 @@ class App extends React.Component{
                     </textarea>
             </div>
                 <div className="col-sm-6">
-                <h1>Résultats</h1>
-                <div>{this.state.text}</div>
+                <div dangerouslySetInnerHTML={this.renderText(this.state.text)}/>
             </div>
             </div>
             </div>
